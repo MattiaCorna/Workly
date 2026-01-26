99ee070ebb968f6c4ff36a564aca38394cb76d65
Corna Mattia <corna.mattia.studente@itispaleocapa.it>
2026-01-26 15:05:17 +0000

    modifiche al avvia.sh

--- avvia.sh
+++ avvia.sh
@@ -1,2 +1,7 @@
 sudo service apache2 start
 sudo service mariadb start
+
+echo "RICORDATI DI APRIRE LA PORTA 80!!!"
+echo "https://potential-space-waddle-97j7vvv69q5j3xp6x-80.app.github.dev/phpmyadmin"
+
+
